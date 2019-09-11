038787ef04b10fa0ba7b54db703a3396faa096c8
Yang,Dongwuk <donghae0414@naver.com>
2019-09-12 00:00:03 +0900

    Make My First Server
    It has cannot get / error
    just make express server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi');
+const express = require('express'); // import(find) express file
+const app = express(); // make express application
+
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`Listening on : https://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
