0892da0c8df0b5632712f8f917e3ea4d7fc92e81
Yang,Dongwuk <donghae0414@naver.com>
2019-09-19 00:41:41 +0900

    apply MVC pattern
    separate Controller and Router

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ import morgan from "morgan"; // morgan improve loging
 import helmet from "helmet"; // for security of nodejs and functionallity
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express(); // make express application
 
-const handleHome = (req, res) => res.send('Hello from home');
-const handleProfile = (req, res) => res.send("You are on my profile");
-
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
